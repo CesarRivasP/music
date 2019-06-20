84a12ac7da32be2dee68dbfdfd168fa41085ef73
CesarRivasP <cesarjose58@gmail.com>
2019-06-19 20:34:51 -0400

    Implemented state and form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect, Fragment } from 'react';
+import Form from './components/form';
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  // Utilizar useState con tres states diferentes
+  const [artist, addArtist] = useState('');
+  const [letter, addLetter] = useState('');
+  const [info, addInfo] = useState('');
+
+  return (
+    <Fragment>
+      <Form />
+    </Fragment>
   );
 }
 
