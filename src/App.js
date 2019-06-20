252574e62a25f87877e85a5df2b11ddf9cd5fea7
CesarRivasP <cesarjose58@gmail.com>
2019-06-19 21:13:02 -0400

    added api url and setLetter

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { useState, useEffect, Fragment } from 'react';
+import axios from 'axios';
 import Form from './components/form';
 
 function App() {
 
   // Utilizar useState con tres states diferentes
   const [artist, addArtist] = useState('');
-  const [letter, addLetter] = useState('');
-  const [info, addInfo] = useState('');
+  const [letter, addLetter] = useState([]);
+  const [info, addInfo] = useState({});
 
   // Metodo para consultar la api de letras de canciones
-  const consultLetterAPI = (search) => {
-    console.log(search);
+  const consultLetterAPI = async(search) => {
+    const { artist, song } = search;
+    const url = `https://api.lyrics.ovh/v1/${artist}/${song}`;
+
+    // consultar la api
+    const result = await axios(url);
+
+    addLetter(result.data.lyrics);
   }
 
   return (
